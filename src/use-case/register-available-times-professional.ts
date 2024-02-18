f8b602ce4d70624a94acacd43fc0c43e89c20a0a
DeyvisonTav <deyvisontav@gmail.com>
2024-02-18 12:15:00 -0300

    feat: use-case available

--- src/use-case/register-available-times-professional.ts
+++ src/use-case/register-available-times-professional.ts
@@ -2,10 +2,12 @@ import { AvailableTime } from "@prisma/client";
 import { AvailableTimeRepository } from "../repositories/available-repository";
 
 interface RegisterAvailableTimesBarberUseCaseRequest {
-  initial_time: string;
-  end_time: string;
-  professionalId: string;
-  day_of_week: string;
+  availableTimes: {
+    initial_time: string;
+    end_time: string;
+    professionalId: string;
+    day_of_week: string;
+  }[];
 }
 
 interface RegisterAvailableTimesBarberUseCaseResponse {
@@ -16,46 +18,52 @@ export class RegisterAvailableTimesBarberUseCase {
   constructor(private availableTimesRepository: AvailableTimeRepository) {}
 
   async execute({
-    professionalId,
-    initial_time,
-    end_time,
-    day_of_week,
+    availableTimes,
   }: RegisterAvailableTimesBarberUseCaseRequest): Promise<RegisterAvailableTimesBarberUseCaseResponse> {
-    var availableTimes: AvailableTime[] = [];
+    const createdAvailableTimes: AvailableTime[] = [];
 
-    const startHours = parseInt(initial_time.split(":")[0]);
-    const startMinutes = parseInt(initial_time.split(":")[1]);
+    for (const {
+      initial_time,
+      end_time,
+      professionalId,
+      day_of_week,
+    } of availableTimes) {
+      const startHours = parseInt(initial_time.split(":")[0]);
+      const startMinutes = parseInt(initial_time.split(":")[1]);
 
-    const endHours = parseInt(end_time.split(":")[0]);
-    const endMinutes = parseInt(end_time.split(":")[1]);
+      const endHours = parseInt(end_time.split(":")[0]);
+      const endMinutes = parseInt(end_time.split(":")[1]);
 
-    const startTime = new Date(1970, 0, 1, startHours, startMinutes);
-    const endTime = new Date(1970, 0, 1, endHours, endMinutes);
+      const startTime = new Date(1970, 0, 1, startHours, startMinutes);
+      const endTime = new Date(1970, 0, 1, endHours, endMinutes);
 
-    const interval = 30 * 60 * 1000; // 30 minutos em milissegundos
+      const interval = 30 * 60 * 1000; // 30 minutos em milissegundos
 
-    for (
-      let currentTime = startTime;
-      currentTime <= endTime;
-      currentTime.setTime(currentTime.getTime() + interval)
-    ) {
-      const _label = currentTime.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      });
+      for (
+        let currentTime = startTime;
+        currentTime <= endTime;
+        currentTime.setTime(currentTime.getTime() + interval)
+      ) {
+        const _label = currentTime.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        });
 
-      const createdAvailableTime = await this.availableTimesRepository.create({
-        label: _label,
-        day_of_week,
-        professionals: { connect: { id: professionalId } },
-      });
+        const createdAvailableTime = await this.availableTimesRepository.create(
+          {
+            label: _label,
+            day_of_week,
+            professionals: { connect: { id: professionalId } },
+          }
+        );
 
-      availableTimes.push(createdAvailableTime);
+        createdAvailableTimes.push(createdAvailableTime);
+      }
     }
 
     return {
-      availableTimes,
+      availableTimes: createdAvailableTimes,
     };
   }
 }
