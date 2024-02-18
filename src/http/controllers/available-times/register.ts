292f4e0c02ad2b488ef84884a9c52bcd47a0fcf7
DeyvisonTav <deyvisontav@gmail.com>
2024-02-18 12:13:41 -0300

    feat: array for available-date register

--- src/http/controllers/available-times/register.ts
+++ src/http/controllers/available-times/register.ts
@@ -4,27 +4,25 @@ import { PrismaAvailableTimeRepository } from "../../../repositories/prisma/avai
 import { RegisterAvailableTimesBarberUseCase } from "../../../use-case/register-available-times-professional";
 
 const registerBodySchema = z.object({
-  initial_time: z.string(),
-  end_time: z.string(),
-  day_of_week: z.string(),
-  professionalId: z.string(),
+  availableTimes: z.array(
+    z.object({
+      initial_time: z.string(),
+      end_time: z.string(),
+      day_of_week: z.string(),
+      professionalId: z.string(),
+    })
+  ),
 });
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   try {
-    const { initial_time, end_time, day_of_week, professionalId } =
-      registerBodySchema.parse(request.body);
+    const { availableTimes } = registerBodySchema.parse(request.body);
 
     const available_timesRepository = new PrismaAvailableTimeRepository();
     const registerAvailableTimeUseCase =
       new RegisterAvailableTimesBarberUseCase(available_timesRepository);
 
-    await registerAvailableTimeUseCase.execute({
-      initial_time,
-      end_time,
-      day_of_week,
-      professionalId,
-    });
+    await registerAvailableTimeUseCase.execute({ availableTimes });
 
     return reply.status(201).send();
   } catch (error) {
