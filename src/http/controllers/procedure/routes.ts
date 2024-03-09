22ae00981be5f07ad88ca4e620016977af12a601
DeyvisonTav <deyvisontav@gmail.com>
2024-03-09 18:18:13 -0300

    feat: added router find-all-procedures

--- src/http/controllers/procedure/routes.ts
+++ src/http/controllers/procedure/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { update } from "./update";
+import { findAll } from "./findAll";
 
 export async function procedureRoutes(app: FastifyInstance) {
   app.post("/register-procedure", register);
   app.put("/update-procedure", update);
+  app.get("/find-all-procedures", findAll);
 }
